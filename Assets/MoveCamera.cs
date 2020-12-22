a3fd2f7fcb83aba0df73ff82fe4eb3ff08b53d77
TushkaN <tushkan.boi.fas@gmail.com>
2020-12-22 13:07:44 +0400

    Fix mobile touch

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -12,7 +12,7 @@ public class MoveCamera : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetMouseButton(0) && !Input.GetMouseButtonDown(0))
         {
             euler[0] = Mathf.Clamp(euler[0]- Input.GetAxis("Mouse Y"), -35, 35);
             euler[1] += Input.GetAxis("Mouse X");
